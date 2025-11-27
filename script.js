f26edf0e0129dbb76dce6939a52392d4054da006
Chinguunee <u.chingunn@gmail.com>
2025-11-27 12:03:02 +0800

    changed game

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const value = prompt("1-10 дотор тоо оруулан таагаарай");
+// const value = prompt("1-10 дотор тоо оруулан таагаарай");
 
-const randomValue = 1;
+// const randomNumber = Math.floor(Math.random() * 10 + 1);
+for (let i = 0; true; i++) {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  value = Number(prompt("1-10 дотор тоо оруулан "));
+  let oroldlogo = i;
+  if (value === 0) break;
 
-// console.log(Math.floor(Math.random() * 10 + 1));
-
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-
-if (value.includes(randomNumber)) {
-  console.log("Zuw taalaa", randomNumber);
-} else console.log("Buruu baina", randomNumber);
+  if (value < randomNumber) {
+    console.log("Жаахан багадаад байна");
+  } else if (value != randomNumber) {
+    console.log("Уг нь зөв тоо нь байнашдээ: ", randomNumber);
+  }
+  if (value > randomNumber) {
+    console.log("Ихийг оруулаад байнаа");
+  } else if (value != randomNumber) {
+    console.log("Уг нь зөв тоо нь: ", randomNumber);
+  }
+  if (value === randomNumber) {
+    console.log(
+      "Зөв таалаа баяр хүргье, Хөөрхнөө",
+      "Зөв тоо нь:",
+      randomNumber,
+      "Нийт оролдлого",
+      oroldlogo
+    );
+    break;
+  }
+}
